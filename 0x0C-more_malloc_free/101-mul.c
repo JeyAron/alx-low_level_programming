874e48e889695edfbf31c690a9c70d0aa49e9294
JeyAron <aronsule@gmail.com>
2024-01-18 08:04:36 -0800

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -199,7 +199,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (*(argv[1]) == '0')
-	argv[2] = iterate_zeroes(argv[1]);
+	argv[1] = iterate_zeroes(argv[1]);
 	if (*(argv[2]) == '0')
 	argv[2] = iterate_zeroes(argv[2]);
 	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
